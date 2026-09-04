b61e3c83370f3154791c80c7c670f0caa15f9ed2
Osama Tello <aosamatello39@gmail.com>
2026-09-04 14:41:31 +0400

    Further tighten shift section spacing

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ export default function HomePage() {
 
         {/* The Shift */}
         <section data-visual-state="3" className="relative overflow-hidden">
-          <div className="container-page relative z-10 border-t border-line pt-14 pb-5 sm:pt-18 sm:pb-6 lg:pt-20 lg:pb-8">
+          <div className="container-page relative z-10 border-t border-line pt-14 pb-0 sm:pt-18 sm:pb-1 lg:pt-20 lg:pb-2">
             <ProblemShift />
           </div>
         </section>
